6adee465426280cd4a9a4c2422b053a17846bb19
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-10-10 14:35:05 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import puppeteer from "puppeteer-core";
-import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer";
 import { load } from "cheerio";
 
 export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
@@ -36,11 +35,7 @@ class LinkedInScrapper {
   }
 
   private async getPageInfo() {
-    const browser = await puppeteer.launch({
-      executablePath: await chromium.executablePath,
-      args: chromium.args,
-      headless: chromium.headless,
-    });
+    const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(0);
 
@@ -56,7 +51,7 @@ class LinkedInScrapper {
     return {
       browser,
       page,
-    };
+    }
   }
 
   async getProfileInfo(): Promise<ProfileInfo> {
