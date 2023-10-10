20826f7c96e1ba75ca4e54d31109387d29947e85
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-10-10 11:46:16 +0100

    handle timeout

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer, {Browser, Page} from "puppeteer";
 import { load } from "cheerio";
 
 export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
@@ -28,6 +28,10 @@ type Skill = string;
 class LinkedInScrapper {
   private cookie: string;
   private profileLink: string;
+  private browserPage: {
+    browser: Browser;
+    page: Page;
+  } | null = null;
 
   constructor(cookie: string, profileLink: string) {
     this.cookie = cookie;
@@ -37,6 +41,7 @@ class LinkedInScrapper {
   private async getPageInfo() {
     const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
 
     await page.setUserAgent(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
@@ -47,17 +52,20 @@ class LinkedInScrapper {
       domain: ".linkedin.com",
     });
 
-    return {
+    this.browserPage = {
       browser,
       page,
-    };
+    }
   }
 
   async getProfileInfo(): Promise<ProfileInfo> {
+    if (!this.browserPage) {
+      await this.getPageInfo();
+    }
     try {
-      const { page, browser } = await this.getPageInfo();
-      await page.goto(this.profileLink, { timeout: 0 });
-      await page.waitForSelector("img", { timeout: 0 });
+      const { page, browser } = this.browserPage as any;
+      await page.goto(this.profileLink);
+      await page.waitForSelector("img");
       const pageContent = await page.content();
       await browser.close();
 
@@ -88,12 +96,13 @@ class LinkedInScrapper {
 
   async getExperiences(): Promise<Experience[]> {
     await delay(1000);
+    if (!this.browserPage) {
+      await this.getPageInfo();
+    }
     try {
-      const { page, browser } = await this.getPageInfo();
-      await page.goto(this.profileLink + "/details/experience", { timeout: 0 });
-      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-        timeout: 0,
-      });
+      const { page, browser } = this.browserPage as any;
+      await page.goto(this.profileLink + "/details/experience");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2");
       const pageContent = await page.content();
       await browser.close();
 
@@ -154,12 +163,13 @@ class LinkedInScrapper {
 
   async getEducations(): Promise<Education[]> {
     await delay(2000);
+    if (!this.browserPage) {
+      await this.getPageInfo();
+    }
     try {
-      const { page, browser } = await this.getPageInfo();
-      await page.goto(this.profileLink + "/details/education", { timeout: 0 });
-      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-        timeout: 0,
-      });
+      const { page, browser } = this.browserPage as any;
+      await page.goto(this.profileLink + "/details/education");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2");
       const pageContent = await page.content();
       await browser.close();
 
@@ -209,12 +219,13 @@ class LinkedInScrapper {
 
   async getSkills(): Promise<Skill[]> {
     await delay(3000);
+    if (!this.browserPage) {
+      await this.getPageInfo();
+    }
     try {
-      const { page, browser } = await this.getPageInfo();
-      await page.goto(this.profileLink + "/details/skills", { timeout: 0 });
-      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-        timeout: 0,
-      });
+      const { page, browser } = this.browserPage as any;
+      await page.goto(this.profileLink + "/details/skills");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2");
       const pageContent = await page.content();
       await browser.close();
 
