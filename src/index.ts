7c1cf2e04b48a0290e83afc77979a2fa39c71102
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-10-10 11:00:04 +0100

    handle exception

--- src/index.ts
+++ src/index.ts
@@ -53,176 +53,192 @@ class LinkedInScrapper {
     };
   }
 
-  async getProfileInfo():Promise<ProfileInfo> {
-    const { page, browser } = await this.getPageInfo();
-    await page.goto(this.profileLink);
-    await page.waitForSelector("img", { timeout: 10000 });
-    const pageContent = await page.content();
-    await browser.close();
+  async getProfileInfo(): Promise<ProfileInfo> {
+    try {
+      const { page, browser } = await this.getPageInfo();
+      await page.goto(this.profileLink);
+      await page.waitForSelector("img", { timeout: 10000 });
+      const pageContent = await page.content();
+      await browser.close();
 
-    let $ = load(pageContent);
+      let $ = load(pageContent);
 
-    const name = $("#ember27").text().trim();
-    const heading = $("#ember28").text().trim();
-    const address = $(".pv-text-details__left-panel.mt2 span.text-body-small")
-      .text()
-      .trim();
-    const about = $(
-      '.pv-shared-text-with-see-more div.inline-show-more-text span[aria-hidden="true"]'
-    )
-      .first()
-      .text()
-      .trim();
-
-    return {
-      name,
-      heading,
-      address,
-      about,
-    };
-  }
-
-  async getExperiences():Promise<Experience[]> {
-    await delay(1000);
-    const { page, browser } = await this.getPageInfo();
-    await page.goto(this.profileLink + "/details/experience");
-    await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-      timeout: 20000,
-    });
-    const pageContent = await page.content();
-    await browser.close();
-
-    let $ = load(pageContent);
-
-    let experiences: {
-      position: string;
-      company: string;
-      duration: string;
-      about: string;
-    }[] = [];
-
-    $(".pvs-entity").each((_, elem) => {
-      const position = $(elem)
-        .find(
-          "div.display-flex.align-items-center.mr1.t-bold span[aria-hidden='true']"
-        )
+      const name = $("#ember27").text().trim();
+      const heading = $("#ember28").text().trim();
+      const address = $(".pv-text-details__left-panel.mt2 span.text-body-small")
+        .text()
+        .trim();
+      const about = $(
+        '.pv-shared-text-with-see-more div.inline-show-more-text span[aria-hidden="true"]'
+      )
         .first()
         .text()
         .trim();
 
-      // For Company
-      const company = $(elem)
-        .find("span.t-14.t-normal span[aria-hidden='true']")
-        .first()
-        .text()
-        .trim();
-
-      // For Duration
-      const duration = $(elem)
-        .find("span.t-14.t-normal.t-black--light span[aria-hidden='true']")
-        .first()
-        .text()
-        .trim();
-
-      // For About
-      const about = $(elem)
-        .find(
-          "div.display-flex.align-items-center.t-14.t-normal.t-black span[aria-hidden='true']"
-        )
-        .first()
-        .text()
-        .trim();
-
-      experiences.push({
-        position,
-        company,
-        duration,
+      return {
+        name,
+        heading,
+        address,
         about,
-      });
-    });
-
-    return experiences;
+      };
+    } catch (err: any) {
+      throw new Error(err);
+    }
   }
 
-  async getEducations():Promise<Education[]> {
+  async getExperiences(): Promise<Experience[]> {
+    await delay(1000);
+    try {
+      const { page, browser } = await this.getPageInfo();
+      await page.goto(this.profileLink + "/details/experience");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
+        timeout: 20000,
+      });
+      const pageContent = await page.content();
+      await browser.close();
+
+      let $ = load(pageContent);
+
+      let experiences: {
+        position: string;
+        company: string;
+        duration: string;
+        about: string;
+      }[] = [];
+
+      $(".pvs-entity").each((_, elem) => {
+        const position = $(elem)
+          .find(
+            "div.display-flex.align-items-center.mr1.t-bold span[aria-hidden='true']"
+          )
+          .first()
+          .text()
+          .trim();
+
+        // For Company
+        const company = $(elem)
+          .find("span.t-14.t-normal span[aria-hidden='true']")
+          .first()
+          .text()
+          .trim();
+
+        // For Duration
+        const duration = $(elem)
+          .find("span.t-14.t-normal.t-black--light span[aria-hidden='true']")
+          .first()
+          .text()
+          .trim();
+
+        // For About
+        const about = $(elem)
+          .find(
+            "div.display-flex.align-items-center.t-14.t-normal.t-black span[aria-hidden='true']"
+          )
+          .first()
+          .text()
+          .trim();
+
+        experiences.push({
+          position,
+          company,
+          duration,
+          about,
+        });
+      });
+
+      return experiences;
+    } catch (err: any) {
+      throw new Error(err);
+    }
+  }
+
+  async getEducations(): Promise<Education[]> {
     await delay(2000);
-    const { page, browser } = await this.getPageInfo();
-    await page.goto(this.profileLink + "/details/education");
-    await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-      timeout: 20000,
-    });
-    const pageContent = await page.content();
-    await browser.close();
-
-    let $ = load(pageContent);
-
-    let educations: {
-      schoolName: string;
-      degreeField: string;
-      duration: string;
-    }[] = [];
-
-    $(".pvs-entity").each((_, elem) => {
-      const schoolName = $(elem)
-        .find(
-          "div.display-flex.align-items-center.mr1.t-bold span[aria-hidden='true']"
-        )
-        .first()
-        .text()
-        .trim();
-
-      // For Degree/Field
-      const degreeField = $(elem)
-        .find("span.t-14.t-normal span[aria-hidden='true']")
-        .first()
-        .text()
-        .trim();
-
-      // For Duration
-      const duration = $(elem)
-        .find("span.t-14.t-normal.t-black--light span[aria-hidden='true']")
-        .first()
-        .text()
-        .trim();
-
-      educations.push({
-        schoolName,
-        degreeField,
-        duration,
+    try {
+      const { page, browser } = await this.getPageInfo();
+      await page.goto(this.profileLink + "/details/education");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
+        timeout: 20000,
       });
-    });
+      const pageContent = await page.content();
+      await browser.close();
 
-    return educations;
+      let $ = load(pageContent);
+
+      let educations: {
+        schoolName: string;
+        degreeField: string;
+        duration: string;
+      }[] = [];
+
+      $(".pvs-entity").each((_, elem) => {
+        const schoolName = $(elem)
+          .find(
+            "div.display-flex.align-items-center.mr1.t-bold span[aria-hidden='true']"
+          )
+          .first()
+          .text()
+          .trim();
+
+        // For Degree/Field
+        const degreeField = $(elem)
+          .find("span.t-14.t-normal span[aria-hidden='true']")
+          .first()
+          .text()
+          .trim();
+
+        // For Duration
+        const duration = $(elem)
+          .find("span.t-14.t-normal.t-black--light span[aria-hidden='true']")
+          .first()
+          .text()
+          .trim();
+
+        educations.push({
+          schoolName,
+          degreeField,
+          duration,
+        });
+      });
+
+      return educations;
+    } catch (err: any) {
+      throw new Error(err);
+    }
   }
 
-  async getSkills():Promise<Skill[]> {
+  async getSkills(): Promise<Skill[]> {
     await delay(3000);
-    const { page, browser } = await this.getPageInfo();
-    await page.goto(this.profileLink + "/details/skills");
-    await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
-      timeout: 20000,
-    });
-    const pageContent = await page.content();
-    await browser.close();
+    try {
+      const { page, browser } = await this.getPageInfo();
+      await page.goto(this.profileLink + "/details/skills");
+      await page.waitForSelector("h2.t-20.t-bold.ph3.pt3.pb2", {
+        timeout: 20000,
+      });
+      const pageContent = await page.content();
+      await browser.close();
 
-    let $ = load(pageContent);
+      let $ = load(pageContent);
 
-    let skills: string[] = [];
+      let skills: string[] = [];
 
-    $(".pvs-entity").each((_, elem) => {
-      const skill = $(elem)
-        .find(
-          "div.display-flex.align-items-center.mr1.hoverable-link-text.t-bold span[aria-hidden='true']"
-        )
-        .first()
-        .text()
-        .trim();
-      if (skill) {
-        skills.push(skill);
-      }
-    });
+      $(".pvs-entity").each((_, elem) => {
+        const skill = $(elem)
+          .find(
+            "div.display-flex.align-items-center.mr1.hoverable-link-text.t-bold span[aria-hidden='true']"
+          )
+          .first()
+          .text()
+          .trim();
+        if (skill) {
+          skills.push(skill);
+        }
+      });
 
-    return skills;
+      return skills;
+    } catch (err: any) {
+      throw new Error(err);
+    }
   }
 }
 
